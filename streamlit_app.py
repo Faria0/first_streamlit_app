5babbc2ad431db7a286de58c6a6b4e76525b56fc
Faria0 <113446243+Faria0@users.noreply.github.com>
2023-03-21 16:16:50 +0000

    Update streamlit_app.py
    Adding a header.
    Getting the json from fruitvice response rather than the status.

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,10 @@ streamlit.dataframe(fruits_to_show)
 
 
 # New section for fruityvice api- requests
+# Header
+streamlit.header('Fruitvice Fruit Advice!')
+
+# Request response
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
